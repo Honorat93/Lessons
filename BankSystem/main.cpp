08dd171cefb2cc721e4e2d923378f311139c577b
Honorat93 <romaricsedogo95@gmail.com>
2024-10-26 16:02:54 +0200

    Initial Commit

---
+++ BankSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BankSystem
+//
+//  Created by Roma SEDOGO on 26/10/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
